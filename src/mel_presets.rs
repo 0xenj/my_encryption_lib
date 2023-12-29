4e4899b6d624017b0e3a3dd32c8d336e10be938c
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-29 12:48:09 +0100

    improve mel_presets, encrypt/decrypt

--- src/mel_presets.rs
+++ src/mel_presets.rs
@@ -1,101 +1,101 @@
 use super::mel::mel;
 
-pub fn mel_1(line: &str) -> String {
-    mel(line, 1)
+pub fn mel_1(line: &str, encrypt: bool) -> String {
+    mel(line, 1, encrypt)
 }
 
-pub fn mel_2(line: &str) -> String {
-    mel(line, 2)
+pub fn mel_2(line: &str, encrypt: bool) -> String {
+    mel(line, 2, encrypt)
 }
 
-pub fn mel_3(line: &str) -> String {
-    mel(line, 3)
+pub fn mel_3(line: &str, encrypt: bool) -> String {
+    mel(line, 3, encrypt)
 }
 
-pub fn mel_4(line: &str) -> String {
-    mel(line, 4)
+pub fn mel_4(line: &str, encrypt: bool) -> String {
+    mel(line, 4, encrypt)
 }
 
-pub fn mel_5(line: &str) -> String {
-    mel(line, 5)
+pub fn mel_5(line: &str, encrypt: bool) -> String {
+    mel(line, 5, encrypt)
 }
 
-pub fn mel_6(line: &str) -> String {
-    mel(line, 6)
+pub fn mel_6(line: &str, encrypt: bool) -> String {
+    mel(line, 6, encrypt)
 }
 
-pub fn mel_7(line: &str) -> String {
-    mel(line, 7)
+pub fn mel_7(line: &str, encrypt: bool) -> String {
+    mel(line, 7, encrypt)
 }
 
-pub fn mel_8(line: &str) -> String {
-    mel(line, 8)
+pub fn mel_8(line: &str, encrypt: bool) -> String {
+    mel(line, 8, encrypt)
 }
 
-pub fn mel_9(line: &str) -> String {
-    mel(line, 9)
+pub fn mel_9(line: &str, encrypt: bool) -> String {
+    mel(line, 9, encrypt)
 }
 
-pub fn mel_10(line: &str) -> String {
-    mel(line, 10)
+pub fn mel_10(line: &str, encrypt: bool) -> String {
+    mel(line, 10, encrypt)
 }
 
-pub fn mel_11(line: &str) -> String {
-    mel(line, 11)
+pub fn mel_11(line: &str, encrypt: bool) -> String {
+    mel(line, 11, encrypt)
 }
 
-pub fn mel_12(line: &str) -> String {
-    mel(line, 12)
+pub fn mel_12(line: &str, encrypt: bool) -> String {
+    mel(line, 12, encrypt)
 }
 
-pub fn mel_13(line: &str) -> String {
-    mel(line, 13)
+pub fn mel_13(line: &str, encrypt: bool) -> String {
+    mel(line, 13, encrypt)
 }
 
-pub fn mel_14(line: &str) -> String {
-    mel(line, 14)
+pub fn mel_14(line: &str, encrypt: bool) -> String {
+    mel(line, 14, encrypt)
 }
 
-pub fn mel_15(line: &str) -> String {
-    mel(line, 15)
+pub fn mel_15(line: &str, encrypt: bool) -> String {
+    mel(line, 15, encrypt)
 }
 
-pub fn mel_16(line: &str) -> String {
-    mel(line, 16)
+pub fn mel_16(line: &str, encrypt: bool) -> String {
+    mel(line, 16, encrypt)
 }
 
-pub fn mel_17(line: &str) -> String {
-    mel(line, 17)
+pub fn mel_17(line: &str, encrypt: bool) -> String {
+    mel(line, 17, encrypt)
 }
 
-pub fn mel_18(line: &str) -> String {
-    mel(line, 18)
+pub fn mel_18(line: &str, encrypt: bool) -> String {
+    mel(line, 18, encrypt)
 }
 
-pub fn mel_19(line: &str) -> String {
-    mel(line, 19)
+pub fn mel_19(line: &str, encrypt: bool) -> String {
+    mel(line, 19, encrypt)
 }
 
-pub fn mel_20(line: &str) -> String {
-    mel(line, 20)
+pub fn mel_20(line: &str, encrypt: bool) -> String {
+    mel(line, 20, encrypt)
 }
 
-pub fn mel_21(line: &str) -> String {
-    mel(line, 21)
+pub fn mel_21(line: &str, encrypt: bool) -> String {
+    mel(line, 21, encrypt)
 }
 
-pub fn mel_22(line: &str) -> String {
-    mel(line, 22)
+pub fn mel_22(line: &str, encrypt: bool) -> String {
+    mel(line, 22, encrypt)
 }
 
-pub fn mel_23(line: &str) -> String {
-    mel(line, 23)
+pub fn mel_23(line: &str, encrypt: bool) -> String {
+    mel(line, 23, encrypt)
 }
 
-pub fn mel_24(line: &str) -> String {
-    mel(line, 24)
+pub fn mel_24(line: &str, encrypt: bool) -> String {
+    mel(line, 24, encrypt)
 }
 
-pub fn mel_25(line: &str) -> String {
-    mel(line, 25)
+pub fn mel_25(line: &str, encrypt: bool) -> String {
+    mel(line, 25, encrypt)
 }
