eff748ae6f0a43af93ab9e283335579a98529074
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-29 17:35:30 +0100

    improve test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -52,129 +52,165 @@ mod integration_tests {
 
     #[test]
     fn test_mel_7() {
-        assert_eq!(mel_presets::mel_7("abc", true), "def");
-        assert_eq!(mel_presets::mel_7("def", false), "abc");
+        assert_eq!(mel_presets::mel_7("abc", true), "hij");
+        assert_eq!(mel_presets::mel_7("hij", false), "abc");
+        assert_eq!(mel_presets::mel_7("à mangé forêt !? ë", true), "h thunl mvyla !? l");
+        assert_eq!(mel_presets::mel_7("h thunl mvyla !? l", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_8() {
-        assert_eq!(mel_presets::mel_8("abc", true), "def");
-        assert_eq!(mel_presets::mel_8("def", false), "abc");
+        assert_eq!(mel_presets::mel_8("abc", true), "ijk");
+        assert_eq!(mel_presets::mel_8("ijk", false), "abc");
+        assert_eq!(mel_presets::mel_8("à mangé forêt !? ë", true), "i uivom nwzmb !? m");
+        assert_eq!(mel_presets::mel_8("i uivom nwzmb !? m", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_9() {
-        assert_eq!(mel_presets::mel_9("abc", true), "def");
-        assert_eq!(mel_presets::mel_9("def", false), "abc");
+        assert_eq!(mel_presets::mel_9("abc", true), "jkl");
+        assert_eq!(mel_presets::mel_9("jkl", false), "abc");
+        assert_eq!(mel_presets::mel_9("à mangé forêt !? ë", true), "j vjwpn oxanc !? n");
+        assert_eq!(mel_presets::mel_9("j vjwpn oxanc !? n", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_10() {
-        assert_eq!(mel_presets::mel_10("abc", true), "def");
-        assert_eq!(mel_presets::mel_10("def", false), "abc");
+        assert_eq!(mel_presets::mel_10("abc", true), "klm");
+        assert_eq!(mel_presets::mel_10("klm", false), "abc");
+        assert_eq!(mel_presets::mel_10("à mangé forêt !? ë", true), "k wkxqo pybod !? o");
+        assert_eq!(mel_presets::mel_10("k wkxqo pybod !? o", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_11() {
-        assert_eq!(mel_presets::mel_11("abc", true), "def");
-        assert_eq!(mel_presets::mel_11("def", false), "abc");
+        assert_eq!(mel_presets::mel_11("abc", true), "lmn");
+        assert_eq!(mel_presets::mel_11("lmn", false), "abc");
+        assert_eq!(mel_presets::mel_11("à mangé forêt !? ë", true), "l xlyrp qzcpe !? p");
+        assert_eq!(mel_presets::mel_11("l xlyrp qzcpe !? p", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_12() {
-        assert_eq!(mel_presets::mel_12("abc", true), "def");
-        assert_eq!(mel_presets::mel_12("def", false), "abc");
+        assert_eq!(mel_presets::mel_12("abc", true), "mno");
+        assert_eq!(mel_presets::mel_12("mno", false), "abc");
+        assert_eq!(mel_presets::mel_12("à mangé forêt !? ë", true), "m ymzsq radqf !? q");
+        assert_eq!(mel_presets::mel_12("m ymzsq radqf !? q", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_13() {
-        assert_eq!(mel_presets::mel_13("abc", true), "def");
-        assert_eq!(mel_presets::mel_13("def", false), "abc");
+        assert_eq!(mel_presets::mel_13("abc", true), "nop");
+        assert_eq!(mel_presets::mel_13("nop", false), "abc");
+        assert_eq!(mel_presets::mel_13("à mangé forêt !? ë", true), "n znatr sberg !? r");
+        assert_eq!(mel_presets::mel_13("n znatr sberg !? r", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_14() {
-        assert_eq!(mel_presets::mel_14("abc", true), "def");
-        assert_eq!(mel_presets::mel_14("def", false), "abc");
+        assert_eq!(mel_presets::mel_14("abc", true), "opq");
+        assert_eq!(mel_presets::mel_14("opq", false), "abc");
+        assert_eq!(mel_presets::mel_14("à mangé forêt !? ë", true), "o aobus tcfsh !? s");
+        assert_eq!(mel_presets::mel_14("o aobus tcfsh !? s", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_15() {
-        assert_eq!(mel_presets::mel_15("abc", true), "def");
-        assert_eq!(mel_presets::mel_15("def", false), "abc");
+        assert_eq!(mel_presets::mel_15("abc", true), "pqr");
+        assert_eq!(mel_presets::mel_15("pqr", false), "abc");
+        assert_eq!(mel_presets::mel_15("à mangé forêt !? ë", true), "p bpcvt udgti !? t");
+        assert_eq!(mel_presets::mel_15("p bpcvt udgti !? t", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_16() {
-        assert_eq!(mel_presets::mel_16("abc", true), "def");
-        assert_eq!(mel_presets::mel_16("def", false), "abc");
+        assert_eq!(mel_presets::mel_16("abc", true), "qrs");
+        assert_eq!(mel_presets::mel_16("qrs", false), "abc");
+        assert_eq!(mel_presets::mel_16("à mangé forêt !? ë", true), "q cqdwu vehuj !? u");
+        assert_eq!(mel_presets::mel_16("q cqdwu vehuj !? u", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_17() {
-        assert_eq!(mel_presets::mel_17("abc", true), "def");
-        assert_eq!(mel_presets::mel_17("def", false), "abc");
+        assert_eq!(mel_presets::mel_17("abc", true), "rst");
+        assert_eq!(mel_presets::mel_17("rst", false), "abc");
+        assert_eq!(mel_presets::mel_17("à mangé forêt !? ë", true), "r drexv wfivk !? v");
+        assert_eq!(mel_presets::mel_17("r drexv wfivk !? v", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_18() {
-        assert_eq!(mel_presets::mel_18("abc", true), "def");
-        assert_eq!(mel_presets::mel_18("def", false), "abc");
+        assert_eq!(mel_presets::mel_18("abc", true), "stu");
+        assert_eq!(mel_presets::mel_18("stu", false), "abc");
+        assert_eq!(mel_presets::mel_18("à mangé forêt !? ë", true), "s esfyw xgjwl !? w");
+        assert_eq!(mel_presets::mel_18("s esfyw xgjwl !? w", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_19() {
-        assert_eq!(mel_presets::mel_19("abc", true), "def");
-        assert_eq!(mel_presets::mel_19("def", false), "abc");
+        assert_eq!(mel_presets::mel_19("abc", true), "tuv");
+        assert_eq!(mel_presets::mel_19("tuv", false), "abc");
+        assert_eq!(mel_presets::mel_19("à mangé forêt !? ë", true), "t ftgzx yhkxm !? x");
+        assert_eq!(mel_presets::mel_19("t ftgzx yhkxm !? x", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_20() {
-        assert_eq!(mel_presets::mel_20("abc", true), "def");
-        assert_eq!(mel_presets::mel_20("def", false), "abc");
+        assert_eq!(mel_presets::mel_20("abc", true), "uvw");
+        assert_eq!(mel_presets::mel_20("uvw", false), "abc");
+        assert_eq!(mel_presets::mel_20("à mangé forêt !? ë", true), "u guhay zilyn !? y");
+        assert_eq!(mel_presets::mel_20("u guhay zilyn !? y", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_21() {
-        assert_eq!(mel_presets::mel_21("abc", true), "def");
-        assert_eq!(mel_presets::mel_21("def", false), "abc");
+        assert_eq!(mel_presets::mel_21("abc", true), "vwx");
+        assert_eq!(mel_presets::mel_21("vwx", false), "abc");
+        assert_eq!(mel_presets::mel_21("à mangé forêt !? ë", true), "v hvibz ajmzo !? z");
+        assert_eq!(mel_presets::mel_21("v hvibz ajmzo !? z", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_22() {
-        assert_eq!(mel_presets::mel_22("abc", true), "def");
-        assert_eq!(mel_presets::mel_22("def", false), "abc");
+        assert_eq!(mel_presets::mel_22("abc", true), "wxy");
+        assert_eq!(mel_presets::mel_22("wxy", false), "abc");
+        assert_eq!(mel_presets::mel_22("à mangé forêt !? ë", true), "w iwjca bknap !? a");
+        assert_eq!(mel_presets::mel_22("w iwjca bknap !? a", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_23() {
-        assert_eq!(mel_presets::mel_23("abc", true), "def");
-        assert_eq!(mel_presets::mel_23("def", false), "abc");
+        assert_eq!(mel_presets::mel_23("abc", true), "xyz");
+        assert_eq!(mel_presets::mel_23("xyz", false), "abc");
+        assert_eq!(mel_presets::mel_23("à mangé forêt !? ë", true), "x jxkdb clobq !? b");
+        assert_eq!(mel_presets::mel_23("x jxkdb clobq !? b", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_24() {
-        assert_eq!(mel_presets::mel_24("abc", true), "def");
-        assert_eq!(mel_presets::mel_24("def", false), "abc");
+        assert_eq!(mel_presets::mel_24("abc", true), "yza");
+        assert_eq!(mel_presets::mel_24("yza", false), "abc");
+        assert_eq!(mel_presets::mel_24("à mangé forêt !? ë", true), "y kylec dmpcr !? c");
+        assert_eq!(mel_presets::mel_24("y kylec dmpcr !? c", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_25() {
-        assert_eq!(mel_presets::mel_25("abc", true), "def");
-        assert_eq!(mel_presets::mel_25("def", false), "abc");
+        assert_eq!(mel_presets::mel_25("abc", true), "zab");
+        assert_eq!(mel_presets::mel_25("zab", false), "abc");
+        assert_eq!(mel_presets::mel_25("à mangé forêt !? ë", true), "z lzmfd enqds !? d");
+        assert_eq!(mel_presets::mel_25("z lzmfd enqds !? d", false), "a mange foret !? e");
     }
 
     #[test]
     fn test_mel_912() {
-        let input = "Héllo World!".to_string();
-        let converted = "Qmsrt Arcvm".to_string();
-        assert!(mel_912::mel_912(&input, true) == "Qmsrt Arcvm ");
-        assert!(mel_912::mel_912(&converted, false) == "Hello World");
+        assert_eq!(mel_912::mel_912("Héllo World!", true), "Qmsrt Arcvm!");
+        assert_eq!(mel_912::mel_912("Qmsrt Arcvm!", false), "Hello World!");
     }
 
     #[test]
     fn test_mel_mdp() {
-        assert_eq!(mel_mdp::mel_mdp("SALUT", "HEY", true), "ZEJBX");
-        assert_eq!(mel_mdp::mel_mdp("ZEJBX", "HEY", false), "SALUT");
+        assert_eq!(mel_mdp::mel_mdp("SaLUt", "HEy", true), "ZeJBx");
+        assert_eq!(mel_mdp::mel_mdp("ZeJBx", "HEy", false), "SaLUt");
     }
 }
