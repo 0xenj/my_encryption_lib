5e3692c62b44361f842acb3816758fce36d04a63
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-31 16:44:11 +0100

    improve err manage

--- src/mel_presets.rs
+++ src/mel_presets.rs
@@ -1,101 +1,101 @@
 use super::mel::mel;
 
-pub fn mel_1(line: &str, encrypt: bool) -> String {
+pub fn mel_1(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 1, encrypt)
 }
 
-pub fn mel_2(line: &str, encrypt: bool) -> String {
+pub fn mel_2(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 2, encrypt)
 }
 
-pub fn mel_3(line: &str, encrypt: bool) -> String {
+pub fn mel_3(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 3, encrypt)
 }
 
-pub fn mel_4(line: &str, encrypt: bool) -> String {
+pub fn mel_4(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 4, encrypt)
 }
 
-pub fn mel_5(line: &str, encrypt: bool) -> String {
+pub fn mel_5(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 5, encrypt)
 }
 
-pub fn mel_6(line: &str, encrypt: bool) -> String {
+pub fn mel_6(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 6, encrypt)
 }
 
-pub fn mel_7(line: &str, encrypt: bool) -> String {
+pub fn mel_7(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 7, encrypt)
 }
 
-pub fn mel_8(line: &str, encrypt: bool) -> String {
+pub fn mel_8(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 8, encrypt)
 }
 
-pub fn mel_9(line: &str, encrypt: bool) -> String {
+pub fn mel_9(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 9, encrypt)
 }
 
-pub fn mel_10(line: &str, encrypt: bool) -> String {
+pub fn mel_10(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 10, encrypt)
 }
 
-pub fn mel_11(line: &str, encrypt: bool) -> String {
+pub fn mel_11(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 11, encrypt)
 }
 
-pub fn mel_12(line: &str, encrypt: bool) -> String {
+pub fn mel_12(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 12, encrypt)
 }
 
-pub fn mel_13(line: &str, encrypt: bool) -> String {
+pub fn mel_13(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 13, encrypt)
 }
 
-pub fn mel_14(line: &str, encrypt: bool) -> String {
+pub fn mel_14(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 14, encrypt)
 }
 
-pub fn mel_15(line: &str, encrypt: bool) -> String {
+pub fn mel_15(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 15, encrypt)
 }
 
-pub fn mel_16(line: &str, encrypt: bool) -> String {
+pub fn mel_16(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 16, encrypt)
 }
 
-pub fn mel_17(line: &str, encrypt: bool) -> String {
+pub fn mel_17(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 17, encrypt)
 }
 
-pub fn mel_18(line: &str, encrypt: bool) -> String {
+pub fn mel_18(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 18, encrypt)
 }
 
-pub fn mel_19(line: &str, encrypt: bool) -> String {
+pub fn mel_19(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 19, encrypt)
 }
 
-pub fn mel_20(line: &str, encrypt: bool) -> String {
+pub fn mel_20(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 20, encrypt)
 }
 
-pub fn mel_21(line: &str, encrypt: bool) -> String {
+pub fn mel_21(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 21, encrypt)
 }
 
-pub fn mel_22(line: &str, encrypt: bool) -> String {
+pub fn mel_22(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 22, encrypt)
 }
 
-pub fn mel_23(line: &str, encrypt: bool) -> String {
+pub fn mel_23(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 23, encrypt)
 }
 
-pub fn mel_24(line: &str, encrypt: bool) -> String {
+pub fn mel_24(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 24, encrypt)
 }
 
-pub fn mel_25(line: &str, encrypt: bool) -> String {
+pub fn mel_25(line: &str, encrypt: bool) -> Result<String, &'static str> {
     mel(line, 25, encrypt)
 }
