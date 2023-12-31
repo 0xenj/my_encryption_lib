33f7743e9d557c460a5e702e4a7e7fac6339a3f5
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-31 16:43:51 +0100

    improve err mel

--- src/mel.rs
+++ src/mel.rs
@@ -1,27 +1,30 @@
 use crate::utils::replace_accented_char;
 
 fn shift_char(letter: char, shift: i32) -> char {
-    let (start, end) = if letter.is_uppercase() { ('A' as u8, 'Z' as u8) } else { ('a' as u8, 'z' as u8) };
-    
-    if letter.is_alphabetic() {
-        let first_ascii = start as i32;
-        let last_ascii = end as i32;
-        let mut shifted_ascii = letter as i32 + shift;
-
-        while shifted_ascii > last_ascii {
-            shifted_ascii -= 26;
-        }
-        while shifted_ascii < first_ascii {
-            shifted_ascii += 26;
-        }
-
-        std::char::from_u32(shifted_ascii as u32).unwrap()
-    } else {
-        letter
+    if !letter.is_alphabetic() {
+        return letter;
     }
+
+    let (start, end) = if letter.is_uppercase() { ('A' as u8, 'Z' as u8) } else { ('a' as u8, 'z' as u8) };
+    let first_ascii = start as i32;
+    let last_ascii = end as i32;
+    let mut shifted_ascii = letter as i32 + shift;
+
+    while shifted_ascii > last_ascii {
+        shifted_ascii -= 26;
+    }
+    while shifted_ascii < first_ascii {
+        shifted_ascii += 26;
+    }
+
+    std::char::from_u32(shifted_ascii as u32).unwrap()
 }
 
-pub(crate) fn mel(line: &str, initial_shift: i32, encrypt: bool) -> String {
+pub(crate) fn mel(line: &str, initial_shift: i32, encrypt: bool) -> Result<String, &'static str> {
+    if line.is_empty() {
+        return Err("Text must not be empty");
+    }
+
     let mut converted = String::new();
     let shift = if encrypt { initial_shift } else { -initial_shift };
 
@@ -34,5 +37,5 @@ pub(crate) fn mel(line: &str, initial_shift: i32, encrypt: bool) -> String {
         }
     }
 
-    converted
+    Ok(converted)
 }
